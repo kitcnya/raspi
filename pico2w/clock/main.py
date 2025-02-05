83211fabe3ec1bf90f93cba8364e15ae20543680
kitcnya <kitcnya@outlook.com>
2025-02-06 07:53:23 +0900

    update the mainloop/task model

--- pico2w/clock/main.py
+++ pico2w/clock/main.py
@@ -6,9 +6,9 @@ import machine, time
 from logging import basicConfig, WARNING, INFO, DEBUG
 logfile = 'main.log'
 logformat = '%(asctime)s %(levelname)7s: %(message)s'
-#basicConfig(filename = logfile, filemode = 'w', format = logformat, level = DEBUG)
-basicConfig(filename = logfile, filemode = 'w', format = logformat, level = INFO)
-#basicConfig(filename = logfile, filemode = 'w', format = logformat, level = WARNING)
+#basicConfig(filename = logfile, filemode = 'a', format = logformat, level = DEBUG)
+basicConfig(filename = logfile, filemode = 'a', format = logformat, level = INFO)
+#basicConfig(filename = logfile, filemode = 'a', format = logformat, level = WARNING)
 
 from logging import getLogger
 logger = getLogger(__name__)
@@ -16,67 +16,84 @@ logger = getLogger(__name__)
 logger.warning('starting up')
 
 class task(object):
-    def __init__(self, interval = 0):
-        self._interval = interval
-        self.record_start()
-        self.record_end()
-        self._alarm = self._start # force interval zero
+    def __init__(self, main):
+        self._main = main
+        self._start = main.start
+        self._end = main.start
+        self._alarm = main.start
     def invoke(self):
-        self.record_start()
+        self._start = self._main.ticks()
         self._invoke()
-        self._end = time.ticks_us()
-    def record_start(self):
-        self._start = time.ticks_us()
-        self.set_alarm(self, self._interval)
-    def record_end(self):
-        self._end = time.ticks_us()
+        self._end = self._main.ticks()
         self._cputime = time.ticks_diff(self._end, self._start)
-    def set_alarm(self, task, after):
-        self._alarm = time.ticks_add(task._start, after)
+        logger.debug('cputime: %s %s' % (self.__class__.__name__, self._cputime))
     def _invoke(self):
         pass
+    def set_alarm(self, task, after):
+        self._alarm = time.ticks_add(task._alarm, after)
+        self._main.append(self)
 
 class mainloop(object):
     def __init__(self):
         self.tasks = list()
+        self.start = self.ticks()
+        self._init()
+    def _init(self):
+        pass
     def append(self, task):
         self.tasks.append(task)
-        logger.info('task: %s' % task.__class__.__name__)
+    def ticks(self):
+        return time.ticks_us()
     def run(self):
         while True:
             self.dispatch()
     def dispatch(self):
-        now = time.ticks_us()
-        for task in self.tasks:
-            if time.ticks_diff(now, task._alarm) < 0:
-                continue
-            task.invoke()
-            logger.debug('cputime: %s %s' % (task.__class__.__name__, task._cputime))
-
-led = machine.Pin("LED", machine.Pin.OUT)
+        queue = self.tasks
+        runnable = list()
+        maxtrigger = 0
+        self.tasks = list()
+        self.start = self.ticks()
+        for task in queue:
+            task._trigger = time.ticks_diff(self.start, task._alarm)
+            if task._trigger < 0:
+                self.append(task)
+            else:
+                if maxtrigger < task._trigger:
+                    maxtrigger = task._trigger
+                runnable.append(task)
+        while len(runnable) > 0:
+            trigger = maxtrigger
+            queue = runnable
+            runnable = list()
+            maxtrigger = 0
+            for task in queue:
+                if task._trigger >= trigger:
+                    task.invoke()
+                    continue
+                if maxtrigger < task._trigger:
+                    maxtrigger = task._trigger
+                runnable.append(task)
 
 class led_on(task):
-    def __init__(self):
-        super().__init__(1000000)
     def _invoke(self):
-        led.value(1)
+        self._main.led.value(1)
+        self._main.off.set_alarm(self, 50000)
+        self.set_alarm(self, 1000000)
 
 class led_off(task):
-    def __init__(self):
-        super().__init__(1000000)
     def _invoke(self):
-        led.value(0)
+        self._main.led.value(0)
 
 class main(mainloop):
-    def __init__(self):
-        super().__init__()
-        self.on = led_on()
-        self.off = led_off()
-        self.append(self.on)
-        self.append(self.off)
-        self.off.set_alarm(self.on, 200000)
+    def _init(self):
+        self.led = machine.Pin("LED", machine.Pin.OUT)
+        self.on = led_on(self)
+        self.off = led_off(self)
+        self.led.value(1)
+        self.off.set_alarm(self.on, 3000000)
+        self.on.set_alarm(self.on, 4000000)
 
 try:
     main().run()
 except Exception as e:
-    logger.error('%s: %s' % (e.__class__.__name__, e.value))
+    logger.critical('%s: %s' % (e.__class__.__name__, e.value))
