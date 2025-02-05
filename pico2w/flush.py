aba91ee4969b661d6882f953958b874afceeac94
kitcnya <kitcnya@outlook.com>
2025-02-06 03:01:21 +0900

    change update sequence (add fetching *.log and initializing rtc)

--- pico2w/flush.py
+++ pico2w/flush.py
@@ -15,6 +15,9 @@ def main():
     if not os.path.exists(directory):
         raise ValueError('No directory: %s' % directory)
     os.chdir(directory)
+
+    sp.run(['mpremote', 'rtc', '--set'])
+
     cmd = sp.run(['mpremote', 'fs', 'ls'], stdout = sp.PIPE)
     coding = 'utf-8'
     out = cmd.stdout.decode(coding)
@@ -26,17 +29,27 @@ def main():
         size = m.group(1)
         name = m.group(2)
         remote.append(name)
+
     local = list()
     for name in os.listdir('.'):
+        if re.fullmatch('.*\.log', name):
+            continue
         local.append(name)
+
+    for name in remote:
+        if re.fullmatch('.*\.log', name):
+            sp.run(['mpremote', 'fs', 'cp', ':' + name, name])
+
+    for name in local:
+        sp.run(['mpremote', 'fs', 'cp', name, ':' + name])
+
     if args.prune:
         for name in remote:
             if name in local:
                 continue
             print('>>> discarding %s on remote' % name)
             sp.run(['mpremote', 'fs', 'rm', name])
-    for name in local:
-        sp.run(['mpremote', 'fs', 'cp', name, ':' + name])
+
     sp.run(['mpremote', 'reset'])
 
 if __name__ == '__main__':
