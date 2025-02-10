6e67409e3ae177273c6c4304af3fec7b209f47f1
kitcnya <kitcnya@outlook.com>
2025-02-11 05:34:54 +0900

    wlan test

--- pico2w/clock/main.py
+++ pico2w/clock/main.py
@@ -3,7 +3,9 @@
 # https://opensource.org/license/mit/
 # SPDX-License-Identifier: MIT
 
+import gc
 import time
+import json
 from task import *
 from led import *
 
@@ -18,7 +20,49 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 def main():
-    morse_demo1().run()
+    #morse_demo1().run()
+    s = sequencer()
+    s.led = led()
+    op = morse(s)
+    op.tone('... - .- .-.- -///') # start
+    gc.collect() # force gc
+    op.set_time()
+    op.task()
+    s.run()
+    logger.info('led ok')
+
+    with open('profile.json') as f:
+        profile = json.load(f)
+
+    logger.info(str(profile))
+
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(profile['wlan']['ssid'], profile['wlan']['pass'])
+
+    while not wlan.isconnected() and wlan.status() >= 0:
+        s.led.on()
+        time.sleep(0.2)
+        s.led.off()
+        time.sleep(0.2)
+        s.led.on()
+        time.sleep(0.2)
+        s.led.off()
+        time.sleep(0.2)
+
+    logger.info('wlan connected')
+    logger.info(str(wlan.ifconfig()))
+
+    ok = morse(s)
+    ok.tone('///--- -.-/...- .-///') # OK VA
+    gc.collect() # force gc
+    ok.set_time()
+    ok.task()
+    s.run()
+
+    wlan.disconnect()
+    logger.info('wlan disconnected')
 
 logger.warning('starting up')
 try:
