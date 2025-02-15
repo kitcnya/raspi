689f7d4e459665f0372690be1073ba5fd9718b7e
kitcnya <kitcnya@outlook.com>
2025-02-16 00:48:41 +0900

    simple clock implementaion

--- pico2w/clock/main.py
+++ pico2w/clock/main.py
@@ -25,21 +25,22 @@ logger = getLogger(__name__)
 class ntpget(object):
 
     def __init__(self, server):
-        self.server = server
-        self.ntpserver = so.getaddrinfo(self.server, 123)[0][-1]
-        self.sock = so.socket(so.AF_INET, so.SOCK_DGRAM, 0)
-        self.sock.settimeout(0.5)
+        self.ntpserver = so.getaddrinfo(server, 123)[0][-1]
         self.request = b'\x23' + 47 * b'\0'
 
     def get(self):
+        sock = so.socket(so.AF_INET, so.SOCK_DGRAM, 0)
+        sock.settimeout(0.5)
         start = time.ticks_us()
-        self.sock.sendto(self.request, self.ntpserver)
         try:
-            answer, src = self.sock.recvfrom(64)
+            sock.sendto(self.request, self.ntpserver)
+            answer, src = sock.recvfrom(64)
         except Exception as e:
             logger.error('%s: %s' % (e.__class__.__name__, e.value))
+            sock.close()
             return 0, start
         end = time.ticks_us()
+        sock.close()
         data = struct.unpack('!12I', answer)
         ntptime = data[10] # transmit timestamp (sec) on server
         if ntptime < 2147483648:
@@ -47,49 +48,44 @@ class ntpget(object):
         else:
             ntptime -= 2147483648 # ntp era 0 (since 20-Jan-1968 03:14:08)
         epoch = ntptime - 61505152
-        ticks = time.ticks_add(start, time.ticks_diff(end, start) // 2)
         usec = data[11] // 4295 # convert transmit timestamp (frac) on server into usec
-        ticks -= usec # adjust machine ticks suit to be epoch
+        ticks = time.ticks_add(start, (time.ticks_diff(end, start) // 2) - usec)
+        epoch += 9 * 3600 # UTC->JST
         return epoch, ticks
 
-def main():
-    #morse_demo1().run()
-    s = sequencer()
-    s.led = led()
-    op = morse(s)
-    op.tone('... - .- .-.- -///') # start
-    gc.collect() # force gc
-    op.set_time()
-    op.task()
-    s.run()
-    logger.info('led ok')
+def rtc_set(epoch): # localtime
+    (year, month, day, hour, minute, second, weekday, yearday) = time.gmtime(epoch)
+    rtc = machine.RTC()
+    rtc.datetime((year, month, day, 0, hour, minute, second, 0))
+    logger.warning('rtc: %d-%02d-%02d %02d:%02d:%02d' % (year, month, day, hour, minute, second))
 
+def rtc_json():
     try:
         with open('rtc.json') as t:
             st = json.load(t)
-        year = st['year']
-        month = st['month']
-        day = st['day']
-        weekday = st['weekday']
-        hour = st['hour']
-        minute = st['minute']
-        second = st['second']
-        subsecond = st['subsecond']
-        rtc = machine.RTC()
-        rtc.datetime((year, month, day, weekday, hour, minute, second, subsecond))
-        logger.warning('rtc.json: %d-%02d-%02d %02d:%02d:%02d' % (year, month, day, hour, minute, second))
+        epoch = st['epoch']
+        epoch += 9 * 3600 # UTC->JST
+        rtc_set(epoch + 1) # XXX: w/ adjust
     except Exception as e:
         logger.error('%s: %s' % (e.__class__.__name__, e.value))
 
+def greeting(s):
+    op = morse(s)
+    op.tone('... - .- .-.- -///') # start
+    op.set_time()
+    op.task()
+    s.run()
+
+def get_profile(s):
     with open('profile.json') as f:
         profile = json.load(f)
-
     logger.info(str(profile))
+    return profile
 
+def wlan_init(s, profile):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(profile['wlan']['ssid'], profile['wlan']['pass'])
-
     while not wlan.isconnected() and wlan.status() >= 0:
         s.led.on()
         time.sleep(0.2)
@@ -99,36 +95,81 @@ def main():
         time.sleep(0.2)
         s.led.off()
         time.sleep(0.2)
-
     logger.info('wlan connected')
     logger.info(str(wlan.ifconfig()))
+    return wlan
 
+def ntp_init(s, profile):
+    n = 0
     ntp = ntpget(profile['ntp']['server'])
-
-    ok = morse(s)
-    ok.tone('///--- -.-/...- .-///') # OK VA
-
-    while True:
-        gc.collect() # force gc
-        op.set_time()
-        op.task()
-        s.run()
-
+    (epoch, ticks) = ntp.get()
+    while epoch == 0:
+        n += 1
+        if n >= 10:
+            raise RuntimeError('no response from ntp server')
+        for i in range(40):
+            s.led.on()
+            time.sleep(1)
+            s.led.off()
+            time.sleep(0.5)
         (epoch, ticks) = ntp.get()
-        if epoch != 0:
-            epoch += 9 * 3600 # UTC->JST
-            (year, month, day, hour, minute, second, weekday, yearday) = time.gmtime(epoch + 1) # XXX: w/ adjust
-            rtc.datetime((year, month, day, 0, hour, minute, second, 0))
-            rticks = time.ticks_us()
-            (ryear, rmonth, rday, weekday, rhour, rminute, rsecond, subsecond) = rtc.datetime()
-            logger.info('ntp: %d-%02d-%02d %02d:%02d:%02d (%s)' % (year, month, day, hour, minute, second, ticks))
-            logger.info('rtc: %d-%02d-%02d %02d:%02d:%02d (%s)' % (ryear, rmonth, rday, rhour, rminute, rsecond, rticks))
+    rtc_set(epoch + 1) # XXX: w/ adjust
+    return ntp, epoch, ticks
 
-        gc.collect() # force gc
-        ok.set_time()
-        ok.task()
-        s.run()
-        time.sleep(360)
+class ntptask(task):
+
+    def task(self):
+        (epoch, ticks) = self.sequencer.ntp.get()
+        epoch += 1
+        if epoch != self.sequencer.clock.epoch:
+            logger.warning('epoch: %s (internal) != %s (ntp)' % (self.sequencer.clock.epoch, epoch))
+            self.sequencer.clock.epoch = epoch
+        d = time.ticks_diff(ticks, self.sequencer.clock.ticks)
+        logger.warning('ticks: %s (internal - ntp)' % d)
+        self.sequencer.clock.set_time(ticks, 1000000)
+
+class clock(task):
+
+    def init(self):
+        self.ntp = ntptask(self.sequencer)
+        self.off = led_off(self.sequencer)
+        self.on2 = led_on(self.sequencer)
+        self.off2 = led_off(self.sequencer)
+
+    def setup(self):
+        self.epoch += 2
+        self.set_time(self.ticks)
+        self.set_alarm(self, 2000000)
+
+    def task(self):
+        self.sequencer.led.on()
+        (year, month, day, hour, minute, second, weekday, yearday) = time.gmtime(self.epoch)
+        if second == 0:
+            self.off.set_alarm(self, 150000)
+            self.on2.set_alarm(self, 300000)
+            self.off2.set_alarm(self, 450000)
+        else:
+            self.off.set_alarm(self, 300000)
+            if second == 2 and minute % 10 == 2:
+                self.ticks = self._alarm
+                self.ntp.set_alarm(self, 310000)
+            elif second == 59:
+                gc.collect() # force gc
+        self.epoch += 1
+        self.set_alarm(self, 1000000)
+
+def main():
+    rtc_json()
+    s = sequencer()
+    s.led = led()
+    s.clock = clock(s)
+    greeting(s)
+    profile = get_profile(s)
+    wlan = wlan_init(s, profile)
+    (s.ntp, s.clock.epoch, s.clock.ticks) = ntp_init(s, profile)
+
+    s.clock.setup()
+    s.run()
 
     wlan.disconnect()
     logger.info('wlan disconnected')
@@ -139,4 +180,4 @@ try:
     logger.critical('mainloop exit.')
 except Exception as e:
     logger.critical('%s: %s' % (e.__class__.__name__, e.value))
-    led().on()
+led().on()
